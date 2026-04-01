e7f04cede9bb094b513ee17e6ce99b657a93b070
KimBuSeong <qntjd262@gmail.com>
2026-04-01 16:50:10 +0900

    infectionCameraEffect Add
    shake, noise, distortion

--- Assets/02. Scripts/Player/Stat/InfectionCameraEffect.cs
+++ Assets/02. Scripts/Player/Stat/InfectionCameraEffect.cs
@@ -1,6 +1,8 @@
 using System.Collections;
-using System.Runtime.InteropServices;
+using DG.Tweening;
+using Unity.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
@@ -16,7 +18,8 @@ public class InfectionCameraEffect : MonoBehaviour
     private enum currentEffectType
     {
         Distortion,
-        Noise
+        Noise,
+        Shake
     }
 
     void Start()
@@ -68,7 +71,7 @@ public class InfectionCameraEffect : MonoBehaviour
         {
             int currentStage = playerStat.currentInfectionStage;
 
-            yield return StartCoroutine(RandomEffectCoroutine());
+            yield return RandomEffectCoroutine();
 
             float waitInterval = 15f;
             if(currentStage == 2) waitInterval = 10f;
@@ -81,32 +84,94 @@ public class InfectionCameraEffect : MonoBehaviour
     //카메라 효과 왜곡, 노이즈 둘 중 하나 랜덤 재생
     private IEnumerator RandomEffectCoroutine()
     {
-        float currentInfection = playerStat.infection.currentValue;
-        float intensityValue = Mathf.Clamp01(0.5f + (currentInfection/100f) * 0.5f);
-
-        currentEffectType selectedEffect = (currentEffectType)Random.Range(0,2);
-        int duration = Random.Range(4, 6);
+        currentEffectType selectedEffect = (currentEffectType)Random.Range(0,3);
 
         switch (selectedEffect)
         {
             case currentEffectType.Distortion:
-                if(lensDistortion != null)
-                {
-                    lensDistortion.intensity.value = -1f * intensityValue;
-                    lensDistortion.scale.value = 1.5f;
-                }
+                yield return DistortionCoroutine();
                 break;
             case currentEffectType.Noise:
-                if(filmGrain != null)
-                {
-                    filmGrain.intensity.value = 1.0f;
-                    filmGrain.response.value = 0f;
-                }
+                yield return NoiseCoroutine();
+                break;
+            case currentEffectType.Shake:
+                yield return ShakeCoroutine();
                 break;
         }
 
-        yield return new WaitForSeconds(duration);
         ResetEffects();
+    }
+
+    #region 카메라 효과 재생 코루틴
+    private IEnumerator DistortionCoroutine()
+    {
+        float intensityValue = GetCurrentIntensity();
+
+        if(lensDistortion != null)
+        {
+            Debug.Log("왜곡 실행");
+            DOTween.To(()=> lensDistortion.intensity.value,x => lensDistortion.intensity.value = x, -1f*intensityValue, 1f).SetId("CameraFX");
+            DOTween.To(() => lensDistortion.scale.value, x => lensDistortion.scale.value = x, 1f + (0.5f * intensityValue), 1f).SetId("CameraFX");
+                
+            yield return new WaitForSeconds(5f);
+                    
+            DOTween.To(()=> lensDistortion.intensity.value,x => lensDistortion.intensity.value = x, 0f, 2f).SetId("CameraFX");
+            DOTween.To(() => lensDistortion.scale.value, x => lensDistortion.scale.value = x, 1f, 2f).SetId("CameraFX");
+
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    private IEnumerator NoiseCoroutine()
+    {
+        float intensityValue = GetCurrentIntensity();
+
+        if(filmGrain != null)
+        {
+            Debug.Log("Noise 실행");
+            DOTween.To(() => filmGrain.intensity.value, x => filmGrain.intensity.value = x, 1f * intensityValue, 1f).SetId("CameraFX");
+            DOTween.To(() => filmGrain.response.value, x => filmGrain.response.value = x, 0.8f - (0.8f * intensityValue), 1f).SetId("CameraFX");
+
+            yield return new WaitForSeconds(5f);
+
+            DOTween.To(() => filmGrain.intensity.value, x => filmGrain.intensity.value = x, 0f, 2f).SetId("CameraFX");
+            DOTween.To(() => filmGrain.response.value, x => filmGrain.response.value = x, 0.8f, 2f).SetId("CameraFX");
+
+            yield return new WaitForSeconds(2f);
+        }
+         
+    }
+
+    private IEnumerator ShakeCoroutine()
+    {
+        Debug.Log("Shake 실행");
+
+        float intensityValue = GetCurrentIntensity();
+
+        
+        float shakeDuration = 3f;
+
+        float currentShakeStrength = 0.05f + (intensityValue * 0.45f);
+        int currentVibrato = Mathf.RoundToInt(10f+intensityValue*40f);
+
+        transform.DOShakePosition(
+            duration : shakeDuration,
+            strength : currentShakeStrength,
+            vibrato : currentVibrato,
+            randomness : 90f,
+            fadeOut : true
+        ).SetId("CameraShakeFX");
+
+        yield return new WaitForSeconds(shakeDuration);
+    }
+
+
+    #endregion
+    
+    private float GetCurrentIntensity()
+    {
+        float currentInfection = playerStat.infection.currentValue;
+        return Mathf.Clamp01(0.5f + (currentInfection/100f) * 0.5f);
     }
 
     private void ResetEffects()
