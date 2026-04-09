269ebf7842573d75f5783ec54d9cbd4442a902db
Lsj <lsj733138@gmail.com>
2026-04-09 17:56:02 +0900

    Feat : 좀비 Chase 에 기능 추가
    Chase 중 적이 안보이게 된다면 해당 위치까지 일정시간 동안 추가로 이동 구현

--- Assets/02. Scripts/Monster/TestMonsterAI.cs
+++ Assets/02. Scripts/Monster/TestMonsterAI.cs
@@ -15,8 +15,9 @@ public class TestMonsterAI : MonoBehaviour
     [SerializeField] private float      _detectAngle = 60f;
     [SerializeField] private Transform _center;
 
-    [Header("Chase 경로 탐색 주기")]
+    [Header("Chase 경로 탐색 주기 및 추가 이동 시간")]
     [SerializeField] private float  _chaseInterval = 0.3f;
+    [SerializeField] private float  _chaseDuration = 3f;
 
     [Header("공격 범위 및 공격 후 대기 시간")]
     [SerializeField] private Vector3 _halfExtents;
@@ -64,13 +65,14 @@ public class TestMonsterAI : MonoBehaviour
                     new Attacked(blackboard) // 피격 노드
                 ),
 
+
                 new SequenceNode
                 (
                     new IsSeeingPlayer(_detectRadius, _detectAngle, _playerLayer, blackboard), // 플레이어를 보고 있는지 확인
 
                     new SelectorNode // 플레이어가 보인다면
                     (
-                        new MemorySequenceNode
+                        new SequenceNode
                         (
                             new IsInAttackRange(blackboard), // 공격 가능 범위 내인지 확인
                             new MemorySequenceNode // 범위 내라면 공격
@@ -80,12 +82,14 @@ public class TestMonsterAI : MonoBehaviour
                             )
                         ),
 
-                        new SelectorNode
-                        (
-                            new ChasePlayer(_chaseInterval, blackboard) // 범위 바깥이라면 추적
-                                                                        // TODO : 플레이어가 시야에서 사라지면 좀 더 이동해 주변 두리번거리게
-                        )
+                        new ChasePlayer(_chaseInterval, blackboard) // 범위 바깥이라면 추적
                     )
+                ),
+
+                new MemorySequenceNode
+                (
+                    new ConditionNode(() => blackboard.HasLostTarget),
+                    new MoveToLastPoint(_chaseDuration, blackboard)
                 ),
 
                 // 정찰 및 대기
