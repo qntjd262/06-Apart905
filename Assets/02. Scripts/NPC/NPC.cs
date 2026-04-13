905daac32b0fa236df9bc21c38d06e7a6cfe681a
kwonkihon <rnjsrlgns914@naver.com>
2026-04-13 15:58:09 +0900

    npc주석처리/씬이름 교체

--- Assets/02. Scripts/NPC/NPC.cs
+++ Assets/02. Scripts/NPC/NPC.cs
@@ -1,50 +1,50 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class NPC : MonoBehaviour, IInteractable
-{
-    public Quest myQuest;
-    private int dialogueIndex = 0;
+// public class NPC : MonoBehaviour, IInteractable
+// {
+//     public Quest myQuest;
+//     private int dialogueIndex = 0;
 
-    public void Interact(PlayerStat player)
-    {
-        if(myQuest == null) return;
+//     public void Interact(PlayerStat player)
+//     {
+//         if(myQuest == null) return;
 
-        //퀘스트 받기 전
-        if(!QuestManager.Instance.activeQuests.Contains(myQuest) && !myQuest.isCompleted)
-        {
-            ShowDialogue(myQuest.beforeAcceptDialogues);
-            QuestManager.Instance.AcceptQuest(myQuest);
-        }
-        //퀘스트 진행중
-        else if(QuestManager.Instance.activeQuests.Contains(myQuest) && !myQuest.isCompleted)
-        {
-            if(myQuest.currentAmount >= myQuest.goalAmount)
-            {
-                ShowDialogue(myQuest.completeDialogues);
-                CompleteQuest();
-            }
-            else
-            {
-                ShowDialogue(myQuest.duringAcceptDialogues);
-            }
-        }
-    }
+//         //퀘스트 받기 전
+//         if(!QuestManager.Instance.activeQuests.Contains(myQuest) && !myQuest.isCompleted)
+//         {
+//             ShowDialogue(myQuest.beforeAcceptDialogues);
+//             QuestManager.Instance.AcceptQuest(myQuest);
+//         }
+//         //퀘스트 진행중
+//         else if(QuestManager.Instance.activeQuests.Contains(myQuest) && !myQuest.isCompleted)
+//         {
+//             if(myQuest.currentAmount >= myQuest.goalAmount)
+//             {
+//                 ShowDialogue(myQuest.completeDialogues);
+//                 CompleteQuest();
+//             }
+//             else
+//             {
+//                 ShowDialogue(myQuest.duringAcceptDialogues);
+//             }
+//         }
+//     }
 
-    void ShowDialogue(string[] dialogues)
-    {
-        foreach(string line in dialogues)
-        {
-            Debug.Log($"{gameObject.name} : {line}");
-        }
-    }
+//     void ShowDialogue(string[] dialogues)
+//     {
+//         foreach(string line in dialogues)
+//         {
+//             Debug.Log($"{gameObject.name} : {line}");
+//         }
+//     }
 
-    void CompleteQuest()
-    {
-        myQuest.isCompleted = true;
-        if (myQuest.type == QuestType.ItemCollection)
-        {
-            InventoryManager.Instance.RemoveItem(myQuest.targetID, myQuest.goalAmount);
-        }
-        QuestManager.Instance.activeQuests.Remove(myQuest);
-    }
-}
+//     void CompleteQuest()
+//     {
+//         myQuest.isCompleted = true;
+//         if (myQuest.type == QuestType.ItemCollection)
+//         {
+//             InventoryManager.Instance.RemoveItem(myQuest.targetID, myQuest.goalAmount);
+//         }
+//         QuestManager.Instance.activeQuests.Remove(myQuest);
+//     }
+// }
