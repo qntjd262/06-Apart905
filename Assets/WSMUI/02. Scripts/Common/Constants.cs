10d5a91f71515df016b49b1d845058e4bdab220d
Woosangmi <wsm3159@gmail.com>
2026-04-16 13:38:38 +0900

    UI 작업물 업데이트
    캔버스 프리팹 일부 ui요소 삭제

--- Assets/WSMUI/02. Scripts/Common/Constants.cs
+++ Assets/WSMUI/02. Scripts/Common/Constants.cs
@@ -4,13 +4,19 @@ public static class Constants
 {
     public enum ESceneType
     {
+        TestMainScene,
+        TestScene,
+        Ending
+
+        
         // Main,
         // Game,
         // Ending
 
-        TestMainScene,
-        TestScene,
-        Ending
+
+        //  TestMainScene,
+        // TestScene,
+        // Ending
     }
     public enum ESaveLoadType
     {
