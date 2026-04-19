2f98be675886196e10f26b34bada62f5e53157f8
jyj1106 <87955838+jyj1106@users.noreply.github.com>
2026-04-19 09:41:41 +0900

    map fetch
    맵 버그 수정(맵 다시제작)
    시간에 따른 빛 변화(태양) 시스템
    맵에 라이트 배치
    최적화 시스템(맵 이동)

--- Assets/02. Scripts/Level/MoveLevel.cs
+++ Assets/02. Scripts/Level/MoveLevel.cs
@@ -8,18 +8,19 @@ public class MoveLevel : MonoBehaviour
      *    계단 중앙에 가면 스크립트가 작동하는 방식
      *    Player스크립트에서 Player의 상태에 따라 위/아래를 구분     
      *    플레이어 외의 요소들이 이동하는것 보단 플레이어가 이동하고 층만 따라가는게 더 효율적이라고 생각함
-     *    */
+     *    층 간격은 prefab기준으로 5.4
+     */
     
     public enum PlayerState         // 임시로 지정
     {
-        MoveUp, MoveDown
+        None, MoveUp, MoveDown
     }
     public PlayerState state;       // 임시로 지정
-    public Vector3 size;
+    public bool check = false;
 
 
-    private GameObject
-        _Level1, _Level2, _Level3;  // 이동시킬 3개의 층들
+    [SerializeField] private GameObject
+        _LevelBott, _LevelMid, _LevelTop;  // 이동시킬 3개의 층들
     private int _currLevel;         // 현재 층 수
 
     private void Start()
@@ -27,38 +28,51 @@ public class MoveLevel : MonoBehaviour
         _currLevel = 9;             // 시작할 때 9층에서 시작
     }
 
-    private void OnTriggerEnter(Collider other)     // 계단 중앙에 들어왔을 때 작동
-    {
-        if(other.CompareTag("Player"))
-        {
-            if(state == PlayerState.MoveUp)
-                MoveUp();
-            else
-                MoveDown();
-        }
-    }
-
-    private void MoveUp()
+    public void MoveUp()
     {
         if(_currLevel < 18)          // 18층은 옥상 구현
         {
             _currLevel++;
+
+            _LevelBott.transform.Translate(0, 5.4f * 3, 0);   // 최하층을 제일 위로
+            IsMoveUp(true);                            
+
             // 층 이동에 따른 오브젝트/몬스터/NPC 비활성화 및 위치 조정 코드 작성
         }
     }
 
-    private void MoveDown()
+    public void MoveDown()
     {
         if(_currLevel > 1)           // 최하층
         {
             _currLevel--;
+
+            _LevelTop.transform.Translate(0, -5.4f * 3, 0);
+            IsMoveUp(false);
+
             // 층 이동에 따른 오브젝트/몬스터/NPC 비활성화 및 위치 조정 코드 작성
         }
     }
 
-    private void OnDrawGizmos()
+    private void IsMoveUp(bool isMoveUp)     // 층 이동 후 층 위치 조정
     {
-        Gizmos.color = Color.green;
-        Gizmos.DrawCube(transform.position + new Vector3(0f, 0.25f, 0f), size);
+        if (isMoveUp)           // 1-2-3 -> 2-3-1
+        {
+            GameObject LvTop = _LevelBott;   // 현재 최상층은 levelbottom
+            GameObject LvMiddle = _LevelTop; // 현재 중간층은 leveltop
+
+            _LevelBott = _LevelMid;         // 최하층을 2로
+            _LevelMid = LvMiddle;           // 중간층을 3으로
+            _LevelTop = LvTop;              // 최상층을 1로
+        }
+        else                    // 1-2-3 -> 3-1-2
+        {
+            GameObject LvBottom = _LevelTop;   // 현재 최하층은 leveltop
+            GameObject LvMiddle = _LevelBott; // 현재 중간층은 levelBottom
+
+            _LevelTop = _LevelMid;         // 최상층을 2로
+            _LevelMid = LvMiddle;          // 중간층을 1로
+            _LevelBott = LvBottom;         // 최하층을 3으로            
+        }
     }
 }
--- Assets/02. Scripts/Level/TimeFlow.cs
+++ Assets/02. Scripts/Level/TimeFlow.cs
@@ -1,16 +1,67 @@
+using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class TimeFlow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /* 시간의 흐름에 따라 Directional Light의 회전과 색깔이 바뀌는 스크립트
+     * 실제 시간 1분 = 게임 시간 1시간
+     * x축은 90도, y축은 0도일 때가 12시
+     * 시간에 따라 Environment Reflection의 Intensity Multiplier도 변경
+     */
+    [SerializeField] private GameObject directionalLight;
+    [SerializeField] private int hours;
+    [SerializeField] private float minutes;
+
+    [SerializeField] private Color dayColor = new Color(0.8f, 0.8f, 0.8f);
+    [SerializeField] private Color nightColor = new Color(0.2f, 0.2f, 0.2f);
+
+    private void LateUpdate()
     {
-        
+        minutes += Time.deltaTime;
+        if (minutes >= 60f)
+        {
+            hours++;
+            minutes = 0f;
+
+            if (hours >= 24)
+                hours = 0;
+        }
+
+        RotateLight();
+        ChangeLightColor();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RotateLight()      // Directional Light 회전
     {
-        
+        float rotationY = (hours + (minutes / 60f)) / 24f * 360;
+
+        directionalLight.transform.rotation = Quaternion.Euler(25f, rotationY - 180f, 0f);
+    }
+
+    private void ChangeLightColor()     // Directional Light 색깔 변경
+    {
+        if (hours >= 5 && hours <= 8)
+        {
+            float t = (hours - 5) + (minutes / 60f);
+            directionalLight.GetComponent<Light>().color = Color.Lerp(nightColor, dayColor, t / 3f);
+            RenderSettings.reflectionIntensity = 0.2f + t / 10f;
+        }
+        else if (hours > 8 && hours < 16)
+        {
+            directionalLight.GetComponent<Light>().color = dayColor;
+            RenderSettings.reflectionIntensity = 0.5f;
+        }   
+        else if (hours >= 16 && hours <= 20)
+        {
+           float t = (hours - 16) + (minutes / 60f);
+            directionalLight.GetComponent<Light>().color = Color.Lerp(dayColor, nightColor, t / 4f);
+            RenderSettings.reflectionIntensity = 0.5f - t / 11f;
+        }
+        else
+        {
+            directionalLight.GetComponent<Light>().color = nightColor;
+            RenderSettings.reflectionIntensity = 0.15f;
+        }            
     }
 }
