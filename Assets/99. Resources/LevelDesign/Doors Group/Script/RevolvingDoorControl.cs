05911d82ed6986ba173d9ffe6e277e8b87ae16da
Lsj <lsj733138@gmail.com>
2026-05-01 16:43:30 +0900

    Feat : 플레이어 문 상호작용 추가
    문 Tag Shelf 설정 필요 및 collider, revolvingDoorContorl, AudioSource 활성화 필요 / Static 체크 해제 필요 /
    navmesh의 agent radius를 0.48로 줄여야 문 통과 가능

--- Assets/99. Resources/LevelDesign/Doors Group/Script/RevolvingDoorControl.cs
+++ Assets/99. Resources/LevelDesign/Doors Group/Script/RevolvingDoorControl.cs
@@ -1,104 +1,75 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class RevolvingDoorControl : MonoBehaviour
+public class RevolvingDoorControl : MonoBehaviour, IInteractable
 {
-    public string rotateAxis = "Y";
-    public float openAngle = 60;
-    public float openSpeed = 250;
-    private bool nowOpen = false;
-    private bool isAct = false;
+    [Header("문 개폐 설정")]
+    public float openAngle = 90;
+    public float openDuration = 1;
+
+    [Header("문 개폐 사운드")]
     public AudioClip open;
     public AudioClip close;
+
     private AudioSource audioSource;
+
+    private Quaternion openRotation;
+    private Quaternion closeRotation;
+
     public bool isOpen = false;
+    private bool isOpening = false;
+
     void Start()
     {
-        audioSource = GetComponentInParent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
+
+        closeRotation = transform.localRotation;
+        openRotation = Quaternion.Euler(
+            transform.localEulerAngles.x,
+            transform.localEulerAngles.y + openAngle,
+            transform.localEulerAngles.z
+        );
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Interact(PlayerStat player)
     {
-        if (isOpen != nowOpen)
-        {
-            if (isOpen)
-            {
-                if (!audioSource.isPlaying && !isAct)
-                {
-                    audioSource.PlayOneShot(open);
-                    isAct = true;
-                }
-                if (rotateAxis == "X" || rotateAxis == "x")
-                {
-                    transform.Rotate(Vector3.right, openSpeed * Time.deltaTime);
-                    if (Mathf.Abs(transform.localEulerAngles.x) >= openAngle && Mathf.Abs(transform.localEulerAngles.x) <= (360 - openAngle))
-                    {
-                        nowOpen = !nowOpen;
-                        isAct = false;
-                    }
+        if (isOpening) return;
 
-                }
-                else if (rotateAxis == "Z" || rotateAxis == "z")
-                {
-                    transform.Rotate(Vector3.forward, openSpeed * Time.deltaTime);
-                    if (Mathf.Abs(transform.localEulerAngles.z) >= openAngle && Mathf.Abs(transform.localEulerAngles.z) <= (360 - openAngle))
-                    {
-                        nowOpen = !nowOpen;
-                        isAct = false;
-                    }
-                }
-                else
-                {
-                    transform.Rotate(Vector3.up, openSpeed * Time.deltaTime);
-                    if (Mathf.Abs(transform.localEulerAngles.y) >= openAngle && Mathf.Abs(transform.localEulerAngles.y) <= (360 - openAngle))
-                    {
-                        nowOpen = !nowOpen;
-                        isAct = false;
-                    }
-                }
+        isOpen = !isOpen;
+        Quaternion targetRotation = isOpen ? openRotation : closeRotation;
+        
+        if (isOpen) OnOpenSound();
+        else OnCloseSound();
 
-            }
-            else
-            {
-                if (!audioSource.isPlaying && !isAct)
-                {
-                    audioSource.PlayOneShot(close);
-                    isAct = true;
-                }
-                if (rotateAxis == "X" || rotateAxis == "x")
-                {
-                    transform.Rotate(Vector3.right, openSpeed * Time.deltaTime * -1);
-                    if (Mathf.Abs(transform.localEulerAngles.x) <= 5)
-                    {
-                        nowOpen = !nowOpen;
-                        transform.localEulerAngles = Vector3.zero;
-                        isAct = false;
-                    }
-                }
-                else if (rotateAxis == "Z" || rotateAxis == "z")
-                {
-                    transform.Rotate(Vector3.forward, openSpeed * Time.deltaTime * -1);
-                    if (Mathf.Abs(transform.localEulerAngles.z) <= 5)
-                    {
-                        nowOpen = !nowOpen;
-                        transform.localEulerAngles = Vector3.zero;
-                        isAct = false;
-                    }
-                }
-                else
-                {
-                    transform.Rotate(Vector3.up, openSpeed * Time.deltaTime * -1);
-                    if (Mathf.Abs(transform.localEulerAngles.y) <= 5)
-                    {
-                        nowOpen = !nowOpen;
-                        transform.localEulerAngles = Vector3.zero;
-                        isAct = false;
-                    }
-                }
-            }
-        }
+
+        StartCoroutine(RotateDoor(targetRotation));
     }
 
+    IEnumerator RotateDoor(Quaternion targetRotation)
+    {
+        isOpening = true;
+
+        float elapsedTime = 0f;
+        Quaternion startRotation = transform.localRotation;
+
+        while (elapsedTime < openDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            transform.localRotation = Quaternion.Slerp(startRotation, targetRotation, elapsedTime / openDuration);
+            yield return null;
+        }
+
+        transform.localRotation = targetRotation;
+        isOpening = false;
+    }
+
+    private void OnOpenSound()
+    {
+        audioSource.PlayOneShot(open);
+    }
+
+    private void OnCloseSound()
+    {
+        audioSource.PlayOneShot(close);
+    }
 }
