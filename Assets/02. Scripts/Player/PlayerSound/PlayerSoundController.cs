7226eb5c2c9e7cb405b406814343e2834c54256c
Lsj <lsj733138@gmail.com>
2026-06-11 16:18:19 +0900

    Feat : 플레이어 피격 및 타격 사운드 관련 추가(일부)

---
+++ Assets/02. Scripts/Player/PlayerSound/PlayerSoundController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayerSoundController : MonoBehaviour
+{
+    [SerializeField] private PlayerSoundSO soundSO;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void PlaySound(AudioClip clip)
+    {
+        _audioSource.clip = clip;
+        _audioSource.Play();
+        Debug.Log(clip);
+    }
+
+    public void OnAttackSound() => PlaySound(soundSO.attackSound);
+    public void OnAttackedSound() => PlaySound(soundSO.attackedSound);
+
+}
